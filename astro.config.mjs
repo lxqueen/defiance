e7c9ae4cd37ab2fbef548bf5853e1e485f85e863
Alexis Queen <alexisqueen@protonmail.com>
2025-04-21 15:21:32 +0100

    Added Castithan and Irathient fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,33 @@ export default defineConfig({
 	experimental: {
 		fonts: [
 			{
+				provider: "local",
+				name: "Castithan",
+				cssVariable: "--font-castithan",
+				variants: [
+					{
+						weight: "normal",
+						style: "normal",
+						src: ["./src/assets/fonts/Castithan.woff2"],
+					},
+				],
+			},
+			{
 				provider: fontProviders.google(),
 				name: "Exo",
 				cssVariable: "--font-exo",
+			},
+			{
+				provider: "local",
+				name: "Irathient",
+				cssVariable: "--font-irathient",
+				variants: [
+					{
+						weight: "normal",
+						style: "normal",
+						src: ["./src/assets/fonts/Irathient.woff2"],
+					},
+				],
 			},
 			{
 				provider: fontProviders.google(),
