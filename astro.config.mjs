95ddecf03bec862aaf8668af2d44d982b8c361a7
Alexis Queen <alexisqueen@protonmail.com>
2025-04-21 15:08:28 +0100

    Imported fonts (currently Montserrat/Exo, in similar vein to the game's typography)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://lxqueen.github.io/defiance/",
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Exo",
+				cssVariable: "--font-exo",
+			},
+			{
+				provider: fontProviders.google(),
+				name: "Montserrat",
+				cssVariable: "--font-montserrat",
+			},
+		],
+	},
 });
