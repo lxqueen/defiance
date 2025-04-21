b28a9b65be3935c7a16455275eacd4342b7dd446
Alexis Queen <alexisqueen@protonmail.com>
2025-04-21 14:12:19 +0100

    Started work on footer & social links

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,27 @@
 export const SITE_NAME = "Lexi's Mods & Miscellany";
+
+interface SocialLink {
+	name: string;
+	value: string;
+	url?: string;
+}
+
+export const SOCIALS: SocialLink[] = [
+	{
+		name: "Discord",
+		value: "lxqueen",
+		url: "https://discordapp.com/channels/@me/188748561343578112/",
+	},
+	{
+		name: "Bluesky",
+		value: "lxqueen.bsky.social",
+		url: "https://bsky.app/profile/lxqueen.bsky.social",
+	},
+	{
+		name: "Defiance",
+		value: "lxqueen",
+	},
+];
+
+export const FAWKES_DISCORD = "https://discord.gg/fawkes";
+export const ARK_ANGELS_DISCORD = "https://discord.gg/dwTwtYH6Hu";
